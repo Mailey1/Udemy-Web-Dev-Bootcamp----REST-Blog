028696804ae27fefb351c51bcf2f6db077af32ff
Mike <mike.bailey122@gmail.com>
2019-12-31 17:38:20 -0800

    Fixed port and IP in app.listen

--- app.js
+++ app.js
@@ -106,7 +106,7 @@ app.delete("/blogs/:id", function(req, res){
     })
 });
 
-app.listen(3000, function(){
+app.listen(process.env.PORT, process.env.IP, function(){
     console.log("Server running on Port 3000");
 });
 
